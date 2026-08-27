4b15b7d338893cb4218c94d30ef5a1aefb29872f
mority <mority@posteo.net>
2026-08-27 19:41:38 +0200

    merge identical branches

--- include/cista/gpu_compat.h
+++ include/cista/gpu_compat.h
@@ -21,11 +21,7 @@
 // macro and the annotations expand to nothing, so headers using them still
 // need no GPU compiler.
 
-#if defined(__HIP__)
-#define CISTA_GPU_DEVICE_COMPILE 1
-#define CISTA_GPU_COMPAT __host__ __device__
-#define CISTA_GPU_DEVICE_COMPAT __device__
-#elif defined(__CUDA_ARCH__)
+#if defined(__HIP__) || defined(__CUDA_ARCH__)
 #define CISTA_GPU_DEVICE_COMPILE 1
 #define CISTA_GPU_COMPAT __host__ __device__
 #define CISTA_GPU_DEVICE_COMPAT __device__
